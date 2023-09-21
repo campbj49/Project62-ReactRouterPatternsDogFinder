cc99f1b046e8281da4f9be156c53c8a3cd888f2c
John Campbell <campbj49@gmail.com>
2023-09-21 13:07:22 -0400

    first commit to GitHub

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/dogs" >
+        <DogList /> // what props will this need?
+      </Route>
+      <Route path="/dogs/:name" >
+        <DogDetails /> // what props will this need?
+      </Route>
+      <Redirect to="/dogs" />
+    </Switch>
+  );
+}
+
+App.defaultProps = {
+  dogs: [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: whiskey,
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!"
+      ]
+    },
+    {
+      name: "Duke",
+      age: 3,
+      src: duke,
+      facts: [
+        "Duke believes that ball is life.",
+        "Duke likes snow.",
+        "Duke enjoys pawing other dogs."
+      ]
+    },
+    {
+      name: "Perry",
+      age: 4,
+      src: perry,
+      facts: [
+        "Perry loves all humans.",
+        "Perry demolishes all snacks.",
+        "Perry hates the rain."
+      ]
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: tubby,
+      facts: [
+        "Tubby is really stupid.",
+        "Tubby does not like walks.",
+        "Angelina used to hate Tubby, but claims not to anymore."
+      ]
+    }
+  ]
+}
+
+export default App;
